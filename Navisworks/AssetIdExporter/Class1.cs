02a8ce8c41cf3eb32ef682b6c18058ecc39c5d02
Whael_ale <joker9757@gmail.com>
2026-06-27 18:07:31 +0800

    refactor: Rename default class to asset export command

--- Navisworks/AssetIdExporter/Class1.cs
+++ Navisworks/AssetIdExporter/Class1.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//namespace groups related classes together.
 namespace AssetIdExporter
 {
     //Module 1
  
-    public class Class1
+    public class AssetExportCommand
     {
+
     }
 }
